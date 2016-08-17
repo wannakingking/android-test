9f939df4e59dcaa8d268dbd0adc9f6ec809bc76d
ctrlor@x.lsrv <x@ctrlor.net>
2016-08-17 19:08:03 +0800

    Fix bugs in the Network Storage demo

--- dataStorage/src/anony/ctrlor/datastorage/NetworkStorageDemo.java
+++ dataStorage/src/anony/ctrlor/datastorage/NetworkStorageDemo.java
@@ -7,30 +7,80 @@ import java.net.URL;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
 
-public class NetworkStorageDemo extends Activity {
+public class NetworkStorageDemo extends Activity implements Runnable{
 
 	private final static String TAG = "NetworkStorageDemo";
+	
+	private static String urlString = "http://220.181.57.217";
+	private String dataString = "";
+	
+	Button btnGo;
+	EditText et;
+	TextView tv;
+	// handle the message from thread
+	final Handler handler = new Handler()
+	{
+		public void handleMessage(Message msg)
+		{
+			if(msg.what == 0x01)
+			{
+				( (TextView)findViewById(R.id.tv_html) ).setText(dataString);
+				//tv.setText(dataString);
+			}
+		}
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_network_storage_demo );
 		
-		getRequest( "http://162.211.181.21");
+		btnGo 	= (Button) findViewById(R.id.btn_go);
+		et		= (EditText) findViewById(R.id.et_address);
+		et.setText(urlString);
+
+		btnGo.setOnClickListener(new Button.OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				urlString = et.getText().toString();
+				if(urlString != "")
+				{
+					Thread thread = new Thread(NetworkStorageDemo.this);
+					thread.start();
+				}
+				else
+				{
+					tv.setText("");
+				}
+				
+			}
+		});
 	}
 	
-	private void getRequest( String urlString )
+	@Override
+	public void run()
 	{
 		try
 		{
-			URL url = new URL( urlString ); 
-			HttpURLConnection conn = ( HttpURLConnection )url.openConnection();
+			HttpURLConnection conn = (HttpURLConnection)new URL(urlString)
+					.openConnection();
+			conn.setConnectTimeout(15000);
+            conn.setReadTimeout(10000);
 			conn.setRequestMethod( "GET" );
-			conn.setRequestProperty( "User-Agent", "Mozilla/5.0" );
 			conn.setDoInput( true );
 
+            // Start the query
+            conn.connect();
 			BufferedReader rd = new BufferedReader( 
 					new InputStreamReader( conn.getInputStream() ));
 
@@ -39,11 +89,16 @@ public class NetworkStorageDemo extends Activity {
 			while(( line = rd.readLine()) != null )
 			{
 				result.append( line );
+				result.append("\n");
 				Log.d( TAG, "line:" + line );
 			}
 			rd.close();
 
-			( (TextView)findViewById( R.id.tv_html ) ).setText( result.toString() );
+			dataString = result.toString();
+			
+			Message msg = new Message();
+			msg.what = 0x01;
+			handler.sendMessage(msg);
 		}
 		catch( Exception e )
 		{
